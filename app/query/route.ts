3405d08492df6f07837eb03ef02d2da3ff2360c0
Amirhossein Zareian <amirhosseinzareian01@gmail.com>
2025-07-08 18:33:39 +0330

    fix: bug with route file

--- app/query/route.ts
+++ app/query/route.ts
@@ -12,14 +12,15 @@
 //   return data;
 // }
 
-// export async function GET() {
-//   // return Response.json({
-//   //   message:
-//   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
-//   // });
-//   try {
-//     return Response.json(await listInvoices());
-//   } catch (error) {
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
+export async function GET() {
+  return Response.json({ message: 'ok' });
+  // return Response.json({
+  //   message:
+  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  // });
+  // try {
+  //   return Response.json(await listInvoices());
+  // } catch (error) {
+  //   return Response.json({ error }, { status: 500 });
+  // }
+}
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -101,17 +101,18 @@
 //   return insertedRevenue;
 // }
 
-// export async function GET() {
-//   try {
-//     const result = await sql.begin((sql) => [
-//       seedUsers(),
-//       seedCustomers(),
-//       seedInvoices(),
-//       seedRevenue(),
-//     ]);
+export async function GET() {
+  return Response.json({ message: 'ok' });
+  // try {
+  //   const result = await sql.begin((sql) => [
+  //     seedUsers(),
+  //     seedCustomers(),
+  //     seedInvoices(),
+  //     seedRevenue(),
+  //   ]);
 
-//     return Response.json({ message: 'Database seeded successfully' });
-//   } catch (error) {
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
+  //   return Response.json({ message: 'Database seeded successfully' });
+  // } catch (error) {
+  //   return Response.json({ error }, { status: 500 });
+  // }
+}
